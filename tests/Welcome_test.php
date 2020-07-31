56459cdd012105fe3fef099b6aed7dac87e89d25
Nozomu Tsuruta <61049074+NozomuTsuruta@users.noreply.github.com>
2020-07-31 12:36:59 +0900

    '初めてのコミット'

---
+++ tests/Welcome_test.php
@@ -0,0 +1,12 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class Welcome_test extends TestCase
+{
+  public function test_index()
+  {
+    $output = $this->request('GET','welcome/index');
+    $this->assertContains('<title>Welcome to CodeIgniter</title>', $output);
+  }
+}
